5c7ad9f22b36e35f7e9da85aba918529e2fc934d
sitanshmehta <s65mehta@uwaterloo.ca>
2025-06-18 00:00:12 -0400

    Clean up tracked files  in .gitignore

---
+++ pendulum/target/debug/deps/pendulum-ed9f5f8621754e41.d
@@ -0,0 +1,5 @@
+/Users/sitanshmehta/fluid-simulator/pendulum/target/debug/deps/pendulum-ed9f5f8621754e41: src/main.rs
+
+/Users/sitanshmehta/fluid-simulator/pendulum/target/debug/deps/pendulum-ed9f5f8621754e41.d: src/main.rs
+
+src/main.rs:
---
+++ pendulum/target/debug/pendulum.d
@@ -0,0 +1,1 @@
+/Users/sitanshmehta/fluid-simulator/pendulum/target/debug/pendulum: /Users/sitanshmehta/fluid-simulator/pendulum/src/main.rs
